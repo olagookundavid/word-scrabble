3268b0d8a31a368bff3c88bdeae8cccc0c40c4c9
David Olagookun <erijesudo@gmail.com>
2023-03-10 10:18:33 +0100

    Initial Commit

---
+++ Word Scrabble/Word_ScrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrabbleApp.swift
+//  Word Scrabble
+//
+//  Created by David OH on 10/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
